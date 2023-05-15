e45c018499ab0596b8d067658f8b5072ef742440
Luc Meric <lhmeric@osmozis.com>
2023-05-15 09:26:03 +0200

    [04]: Added 'mult.asm' program

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,43 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  // Initialize required variables (i: counter, mul: multiplication result)
+@mul
+M=0
+
+@i
+M=0
+
+  // Our ALU does not provide the multiplication operation
+  // so we need to add R0 R1 times
+(LOOP)
+  // if ( i >= R1 ) break;
+  @i
+  D=M
+  @R1
+  D=D-M
+  @STOP
+  D;JGE
+
+  @i
+  M=M+1
+
+  @R0
+  D=M
+  @mul
+  M=M+D
+
+  @LOOP
+  0;JMP
+
+  // Stop the loop after R1 iterations, then perform R2 = mul
+(STOP)
+  @mul
+  D=M
+  @R2
+  M=D
+
+  // Infinite loop to end the program
+(END)
+  @END
+  0;JMP
